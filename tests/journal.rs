f46d1473cc89803e2a04f3498cd07c7e3373ca9f
Sebastian Wiesner <sebastian@swsnr.de>
2021-06-01 12:58:17 +0200

    Randomize journal log tags in integration tests
    Make sure that we do not accidentally read entries from previous tests
    in case the kernel re-uses the PID of the current test run for another
    test run (unlikely perhaps, but just in case).

--- tests/journal.rs
+++ tests/journal.rs
@@ -1,23 +1,35 @@
 use std::process::Command;
 
 use libsystemd::logging::Priority;
+use rand::distributions::Alphanumeric;
+use rand::Rng;
 use std::collections::HashMap;
-use std::ffi::OsStr;
 
-/// Read from journal.
+fn random_name(prefix: &str) -> String {
+    format!(
+        "{}_{}",
+        prefix,
+        rand::thread_rng()
+            .sample_iter(&Alphanumeric)
+            .take(10)
+            .map(char::from)
+            .collect::<String>()
+    )
+}
+
+/// Read from journal with `journalctl`.
 ///
-/// `matches` are additional matches to filter journal entries.
-fn read_from_journal<I, S>(matches: I) -> Vec<HashMap<String, String>>
-where
-    I: IntoIterator<Item = S>,
-    S: AsRef<OsStr>,
-{
+/// `test_name` is the randomized name of the test being run, and gets
+/// added as `TEST_NAME` match to the `journalctl` call, to make sure to
+/// only select journal entries originating from and relevant to the
+/// current test.
+fn read_from_journal(test_name: &str) -> Vec<HashMap<String, String>> {
     let stdout = String::from_utf8(
         Command::new("journalctl")
             .args(&["--user", "--output=json"])
             // Filter by the PID of the current test process
             .arg(format!("_PID={}", std::process::id()))
-            .args(matches)
+            .arg(format!("TEST_NAME={}", test_name))
             .output()
             .unwrap()
             .stdout,
@@ -32,59 +44,62 @@ where
 
 #[test]
 fn simple_message() {
+    let test_name = random_name("simple_message");
     libsystemd::logging::journal_send(
         Priority::Info,
         "Hello World",
         vec![
-            ("TEST_NAME", "simple_message"),
+            ("TEST_NAME", test_name.as_str()),
             ("FOO", "another piece of data"),
         ]
         .into_iter(),
     )
     .unwrap();
 
-    let messages = read_from_journal(&["TEST_NAME=simple_message"]);
+    let messages = read_from_journal(&test_name);
     assert_eq!(messages.len(), 1);
 
     let message = &messages[0];
     assert_eq!(message["MESSAGE"], "Hello World");
-    assert_eq!(message["TEST_NAME"], "simple_message");
+    assert_eq!(message["TEST_NAME"], test_name);
     assert_eq!(message["PRIORITY"], "6");
     assert_eq!(message["FOO"], "another piece of data");
 }
 
 #[test]
 fn multiline_message() {
+    let test_name = random_name("multiline_message");
     libsystemd::logging::journal_send(
         Priority::Info,
         "Hello\nMultiline\nWorld",
-        vec![("TEST_NAME", "multiline_message")].into_iter(),
+        vec![("TEST_NAME", test_name.as_str())].into_iter(),
     )
     .unwrap();
 
-    let messages = read_from_journal(&["TEST_NAME=multiline_message"]);
+    let messages = read_from_journal(&test_name);
     assert_eq!(messages.len(), 1);
 
     let message = &messages[0];
     assert_eq!(message["MESSAGE"], "Hello\nMultiline\nWorld");
-    assert_eq!(message["TEST_NAME"], "multiline_message");
+    assert_eq!(message["TEST_NAME"], test_name);
     assert_eq!(message["PRIORITY"], "6");
 }
 
 #[test]
 fn multiline_message_trailing_newline() {
+    let test_name = random_name("multiline_message_trailing_newline");
     libsystemd::logging::journal_send(
         Priority::Info,
         "A trailing newline\n",
-        vec![("TEST_NAME", "multiline_message_trailing_newline")].into_iter(),
+        vec![("TEST_NAME", test_name.as_str())].into_iter(),
     )
     .unwrap();
 
-    let messages = read_from_journal(&["TEST_NAME=multiline_message_trailing_newline"]);
+    let messages = read_from_journal(&test_name);
     assert_eq!(messages.len(), 1);
 
     let message = &messages[0];
     assert_eq!(message["MESSAGE"], "A trailing newline\n");
-    assert_eq!(message["TEST_NAME"], "multiline_message_trailing_newline");
+    assert_eq!(message["TEST_NAME"], test_name);
     assert_eq!(message["PRIORITY"], "6");
 }
