320c4c855ed88c24bddc03cb785e6453836a7c3b
David Freese <freese@google.com>
2022-12-15 18:52:06 -0800

    errors: add internal helpers to simplify error handling
    This adds a Context trait, as well as implementations of it for Result
    and Optional to help consolidate the map_err(|e| format!(...)) logic
    that is used to provide propagate error information.  This is inspired
    by anyhow, but returns SdError instead to keep the API surface of the
    library to a single struct within the library.

--- src/errors.rs
+++ src/errors.rs
@@ -1,7 +1,7 @@
-use thiserror::Error;
+use std::fmt::Display;
 
 /// Library errors.
-#[derive(Error, Debug)]
+#[derive(thiserror::Error, Debug)]
 #[error("libsystemd error: {msg}")]
 pub struct SdError {
     pub(crate) kind: ErrorKind,
@@ -31,4 +31,56 @@ impl From<String> for SdError {
 pub(crate) enum ErrorKind {
     Generic,
     SysusersUnknownType,
+}
+
+/// Context is similar to anyhow::Context, in that it provides a mechanism internally to adapt
+/// errors from systemd into SdError, while providing additional context in a readable manner.
+pub(crate) trait Context<T, E> {
+    /// Prepend the error with context.
+    fn context<C>(self, context: C) -> Result<T, SdError>
+    where
+        C: Display + Send + Sync + 'static;
+
+    /// Prepend the error with context that is lazily evaluated.
+    fn with_context<C, F>(self, f: F) -> Result<T, SdError>
+    where
+        C: Display + Send + Sync + 'static,
+        F: FnOnce() -> C;
+}
+
+impl<T, E> Context<T, E> for Result<T, E>
+where
+    E: std::error::Error + Send + Sync + 'static,
+{
+    fn context<C>(self, context: C) -> Result<T, SdError>
+    where
+        C: Display + Send + Sync + 'static,
+    {
+        self.map_err(|e| format!("{}: {}", context, e).into())
+    }
+
+    fn with_context<C, F>(self, context: F) -> Result<T, SdError>
+    where
+        C: Display + Send + Sync + 'static,
+        F: FnOnce() -> C,
+    {
+        self.map_err(|e| format!("{}: {}", context(), e).into())
+    }
+}
+
+impl<T> Context<T, core::convert::Infallible> for Option<T> {
+    fn context<C>(self, context: C) -> Result<T, SdError>
+    where
+        C: Display + Send + Sync + 'static,
+    {
+        self.ok_or_else(|| format!("{}", context).into())
+    }
+
+    fn with_context<C, F>(self, context: F) -> Result<T, SdError>
+    where
+        C: Display + Send + Sync + 'static,
+        F: FnOnce() -> C,
+    {
+        self.ok_or_else(|| format!("{}", context()).into())
+    }
 }
